6b6970e02312236351103563b04ec8f07cf07598
Shailesh Parmar <80666811+shaileshdeuex@users.noreply.github.com>
2021-03-17 10:46:25 +0530

    created card logic (#1)
    * created card logic
    
    * removed logic from card component and added in to board component & fixed some UI
    
    * permoance improved
    
    * Remove unwanted code and un-used variable from card
    
    * solve the problem of 1 and added comments in code

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Card from "./Card";
 
 const cardValue = [];
-const cardLevel = 6;
+const cardLevel = 8;
 let card = [];
 
 // creating values of the card
@@ -10,10 +10,12 @@ const createCardValue = (cardLevel) => {
   for (let i = 1; i <= cardLevel; i++) {
     cardValue.push(Math.ceil(i / 2));
   }
-  // console.log(cardValue);
 };
 
-createCardValue(cardLevel);
+// shuffle the cardValue array
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
 
 // creating card
 const createCards = (cardValue) => {
@@ -21,34 +23,101 @@ const createCards = (cardValue) => {
     card.push({
       flip: false,
       value: cardValue[i],
+      id: i, // id updated
     });
   }
-  // console.log(card);
 };
 
-createCards(cardValue);
+const createGame = () => {
+  createCardValue(cardLevel);
+  shuffle(cardValue);
+  createCards(cardValue);
+};
+
+createGame();
 
 function Board() {
-  // createCards(cardValue);
-
   const [cardDeck, setCardDeck] = useState(card);
+  const [compareCardArr, setCompareCardArr] = useState({});
+  const [gameOver, setGameOver] = useState(false);
+  const [pairCounter, setPairCounter] = useState(1);
 
-  const changeCardDeck = (deck) => {
-    setCardDeck(deck);
+  // method to check {} is empty or not
+  function isObjectEmpty(value) {
+    return (
+      Object.prototype.toString.call(value) === "[object Object]" &&
+      JSON.stringify(value) === "{}"
+    );
+  }
+  // function to check game is ended or not
+  const gameEnd = () => {
+    if (pairCounter >= cardLevel / 2) {
+      setGameOver(true);
+    }
+  };
+
+  const setCompareArr = (newState) => {
+    // updating CardDeck value according to new state value
+    setCardDeck(
+      cardDeck.map((item, id) => {
+        if (id === newState.id) {
+          item.flip = newState.flip;
+          return item;
+        } else {
+          return item;
+        }
+      })
+    );
+
+    // comparring previous click with new click and assigning value accordingly
+    setCompareCardArr((preState) => {
+      if (isObjectEmpty(preState)) {
+        return newState; // prev state is empty so returing new state value
+      } else if (preState.id === newState.id) {
+        return preState; // prev click and new click on same card, returning same value
+      } else if (preState.value === newState.value) {
+        setPairCounter(pairCounter + 1); // increasing pairCounter if preValue and newValue is same.
+        console.log("Match Found", pairCounter);
+        gameEnd();
+        return {};
+      } else {
+        setTimeout(() => {
+          // setting card value to previous state as pair is not found
+          setCardDeck(
+            cardDeck.map((item, id) => {
+              if (id === preState.id || id === newState.id) {
+                item.flip = false;
+                return item;
+              } else {
+                return item;
+              }
+            })
+          );
+        }, 500);
+        return {};
+      }
+    });
   };
 
   return (
-    <div className="board">
-      {cardDeck.map((item, id) => (
-        <Card
-          key={id}
-          id={id}
-          cardDeck={cardDeck}
-          {...item}
-          changeCardDeck={changeCardDeck}
-        />
-      ))}
-    </div>
+    <>
+      <header>
+        <h2 className="board-heading">
+          {gameOver ? "Congratulation you Won!" : "Memory Game!"}
+        </h2>
+      </header>
+      <div className="board">
+        {cardDeck.map((item, id) => (
+          <Card
+            key={id}
+            cardDeck={cardDeck}
+            {...item}
+            compareCardArr={compareCardArr}
+            setCompareArr={setCompareArr}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,40 +1,19 @@
-let compareCardArr = [];
-
 function Card(props) {
-  let { id, flip, value, changeCardDeck } = props;
-  let cardDeck = [...props.cardDeck];
-
-  const compareCard = (cardArr) => {
-    let [card1, card2] = cardArr;
-    console.log(card1, card2);
-    if (card1.value === card2.value) {
-      console.log("Match Found");
-    } else {
-      setTimeout(() => {
-        cardDeck[card1.id].flip = false;
-        cardDeck[card2.id].flip = false;
-        console.log("Not Match");
-        changeCardDeck(cardDeck);
-      }, 1000);
-    }
-
-    compareCardArr = [];
-  };
+  let { id, flip, value, setCompareArr } = props;
 
   const handleClick = (id) => {
-    cardDeck[id].flip = true;
-    compareCardArr.push({ ...cardDeck[id], id });
-    console.log(compareCardArr);
-    if (compareCardArr.length === 2) {
-      compareCard(compareCardArr);
-    }
-    changeCardDeck(cardDeck);
+    setCompareArr({ id, value, flip: true });
   };
 
   return (
     <div className="flip-card" onClick={() => handleClick(id)} id={id}>
       <div className={`flip-card-inner  ${flip ? "flip" : "no-flip"}`}>
-        <div className="flip-card-front"></div>
+        <div className="flip-card-front">
+          <img
+            src="https://image.shutterstock.com/image-illustration/try-your-luck-text-on-600w-1865733352.jpg"
+            alt=""
+          />
+        </div>
         <div className="flip-card-back">
           <h1>{value}</h1>
         </div>
