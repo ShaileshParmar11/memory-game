7d2763f032135e094ef565757ca85a79dae70f65
Shailesh Parmar <80666811+shaileshdeuex@users.noreply.github.com>
2021-03-18 10:36:52 +0530

    Reset button and Score display feature. (#2)
    * added reset and score display functionality, fix some UI related changes and added responsive UI
    
    * functionality improvement done
    
    * Added play again functionality and winner gif for end of game and some code improvement
    
    * createCardValue function placed to under board component

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,57 +1,63 @@
 import { useState } from "react";
 import Card from "./Card";
 
-const cardValue = [];
-const cardLevel = 8;
-let card = [];
-
-// creating values of the card
-const createCardValue = (cardLevel) => {
-  for (let i = 1; i <= cardLevel; i++) {
-    cardValue.push(Math.ceil(i / 2));
-  }
-};
-
 // shuffle the cardValue array
 function shuffle(array) {
   array.sort(() => Math.random() - 0.5);
 }
 
-// creating card
-const createCards = (cardValue) => {
-  for (let i = 0; i < cardValue.length; i++) {
-    card.push({
-      flip: false,
-      value: cardValue[i],
-      id: i, // id updated
-    });
-  }
-};
-
-const createGame = () => {
-  createCardValue(cardLevel);
-  shuffle(cardValue);
-  createCards(cardValue);
-};
-
-createGame();
+// method to check {} is empty or not
+function isObjectEmpty(value) {
+  return (
+    Object.prototype.toString.call(value) === "[object Object]" &&
+    JSON.stringify(value) === "{}"
+  );
+}
 
 function Board() {
+  const cardLevel = 16;
+  const card = [];
+
+  // initialinzing state
   const [cardDeck, setCardDeck] = useState(card);
   const [compareCardArr, setCompareCardArr] = useState({});
   const [gameOver, setGameOver] = useState(false);
+  const [heading, setHeading] = useState("Memory Game!");
   const [pairCounter, setPairCounter] = useState(1);
+  const [movesCounter, setMovesCounter] = useState(0);
 
-  // method to check {} is empty or not
-  function isObjectEmpty(value) {
-    return (
-      Object.prototype.toString.call(value) === "[object Object]" &&
-      JSON.stringify(value) === "{}"
-    );
-  }
+  // creating values of the card
+  const createCardValue = (cardLevel) => {
+    for (let i = 1; i <= cardLevel; i++) {
+      card.push({
+        flip: false,
+        value: Math.ceil(i / 2),
+      });
+    }
+  };
+
+  createCardValue(cardLevel); // generating card value
+  shuffle(card); //initial game start
+
+  // resetting game state on game over and on reset button click
+  const restartGame = () => {
+    resetGame();
+    setHeading("Memory Game!");
+    setGameOver(false);
+  };
+
+  const resetGame = () => {
+    shuffle(card);
+    setCardDeck(card);
+    setCompareCardArr({});
+    setPairCounter(1);
+    setMovesCounter(0);
+  };
+
   // function to check game is ended or not
   const gameEnd = () => {
     if (pairCounter >= cardLevel / 2) {
+      setHeading("Congratulation you Won!");
       setGameOver(true);
     }
   };
@@ -77,10 +83,12 @@ function Board() {
         return preState; // prev click and new click on same card, returning same value
       } else if (preState.value === newState.value) {
         setPairCounter(pairCounter + 1); // increasing pairCounter if preValue and newValue is same.
+        setMovesCounter(movesCounter + 1);
         console.log("Match Found", pairCounter);
         gameEnd();
         return {};
       } else {
+        setMovesCounter(movesCounter + 1);
         setTimeout(() => {
           // setting card value to previous state as pair is not found
           setCardDeck(
@@ -101,21 +109,40 @@ function Board() {
 
   return (
     <>
-      <header>
-        <h2 className="board-heading">
-          {gameOver ? "Congratulation you Won!" : "Memory Game!"}
-        </h2>
+      <header className="board-header">
+        <h2 className="board-heading score">Moves: {movesCounter}</h2>
+        <h2 className="board-heading">{heading}</h2>
+
+        <button className="btn" onClick={resetGame} disabled={gameOver}>
+          Reset
+        </button>
       </header>
-      <div className="board">
-        {cardDeck.map((item, id) => (
-          <Card
-            key={id}
-            cardDeck={cardDeck}
-            {...item}
-            compareCardArr={compareCardArr}
-            setCompareArr={setCompareArr}
-          />
-        ))}
+      <div className="board-body">
+        {gameOver ? (
+          <div className="winnerContainer">
+            <iframe
+              title="winner Gif"
+              src="https://giphy.com/embed/l0HlSDiA6WUytl9oA"
+              className="winnerGif"
+            ></iframe>
+            <button className="btn" onClick={restartGame}>
+              Play Again
+            </button>
+          </div>
+        ) : (
+          <div className="board-game">
+            {cardDeck.map((item, id) => (
+              <Card
+                key={id}
+                cardDeck={cardDeck}
+                {...item}
+                id={id}
+                compareCardArr={compareCardArr}
+                setCompareArr={setCompareArr}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
